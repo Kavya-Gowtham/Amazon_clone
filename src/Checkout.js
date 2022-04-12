912c1d68a5de8a48c6b31333c0368539025d2d3b
Kavya-Gowtham <62173639+Kavya-Gowtham@users.noreply.github.com>
2022-04-12 17:29:33 +0530

    Added code of remove item from basket and showing car items in checkout page and amount sum in subtotal

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import "./Checkout.css";
+import { useStateValue } from './StateProvider';
 import Subtotal from "./Subtotal";
+import CheckoutProduct from './CheckoutProduct'
+
 function Checkout() {
+    const [{basket, dispatch}] = useStateValue();
     return (
         <div className="checkout">
             <div className="checkout_left">
                 <img className="checkout_ad" src="https://m.media-amazon.com/images/G/01/AdProductsWebsite/images/AUX/ILB_BrightColors_NotApproved._TTW_.jpg " alt="" />
             <div>
                 <h2 className="checkout_title"> Your shopping Basket</h2>
+                {basket.map(item => (
+                    <CheckoutProduct
+                    id = {item.id}
+                    title = {item.title}
+                    image = {item.image}
+                    price = {item.price}
+                    rating = {item.rating}
+                    />
+                    
+                ))}
             </div>
             </div> 
             <div className="checkout_right">
--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,8 @@
 export const initialState = {
     basket: [],
 };
+export  const getBasketTotal = (basket) =>
+basket?.reduce((amount,item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
     console.log(action);
@@ -10,6 +12,22 @@ const reducer = (state, action) => {
                 ...state,
                 basket: [...state.basket, action.item],
             }
+            case "Remove_From_Basket" :
+                const index = state.basket.findIndex(
+                    (basketItem) => basketItem.id === action.id
+                );
+                let newBasket = [...state.basket];
+                if(index >= 0){
+                    newBasket.splice(index, 1);
+                }else{
+                    console.warn(
+                        `can't remove product (id: ${action.id}) as its not in basket!`
+                    )
+                }
+                return{
+                    ...state,
+                    basket: newBasket
+                }
         default:
             return state;    
     }
